1a1947944d4a1aa2e3f72a09d579ffcfba459739
liwanliang <396257680@qq.com>
2016-11-18 14:16:47 +0800

    增加可自由选择文件或目录的选择对话框

--- sdk/ui/PiUITool.h
+++ sdk/ui/PiUITool.h
@@ -9,4 +9,10 @@ class CPIUITool
 public:
 	static tstring GetOneDragFilePath(const HDROP& hd);
 	static tstring SelectDirectory(HWND hParent, tcpchar szBeginDir, tcpchar szTitle);
+	/************************************************************************
+		fun:	弹出选择对话框，  可选择文件或目录
+		param:	szBeginDir, 弹窗后显示的起始目录， 如果目录不存在则显示上次的目录
+		memo:	
+	************************************************************************/
+	static tstring SelectFileOrDir(HWND hParent, tcpchar szBeginDir, tcpchar szTitle, tcpchar szBtnOkName);
 };
