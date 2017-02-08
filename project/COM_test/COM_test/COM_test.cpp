8bfa543c267c5900bd3e712165778489fceca70f
lwlTom <396257680@qq.com>
2017-02-08 18:42:14 +0800

    第一个COM组件demo

--- project/COM_test/COM_test/COM_test.cpp
+++ project/COM_test/COM_test/COM_test.cpp
@@ -6,21 +6,44 @@
 #include "Create.h"
 #include "..\COMDll\interface.h"
 using namespace std;
+void trace(const char* szMsg){ cout << szMsg << endl; }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	::CoInitialize(NULL);
 	HRESULT hr = S_FALSE;
-	IUnknown* pIUnknown = CallCreateInstance("COMDll.dll");
-	cout << "get a IUnknown pointer" << endl;
+	//IUnknown* pIUnknown = CallCreateInstance("COMDll.dll");
 	IX* pIX = nullptr;
-	hr = pIUnknown->QueryInterface(IID_IX, (void**)&pIX);
+	bool bClassFactoryDirect = true;
+	if (bClassFactoryDirect)
+	{
+		IClassFactory* pICF = nullptr;
+		hr = CoGetClassObject(CLSID_Component1, CLSCTX_INPROC_SERVER, NULL, IID_IClassFactory, (void**)&pICF);
+		pICF->CreateInstance(NULL, IID_IX, (void**)&pIX);
+		pICF->Release();
+	}
+	else
+	{
+		hr = ::CoCreateInstance(CLSID_Component1, nullptr, CLSCTX_INPROC_SERVER, IID_IX, (void**)&pIX);
+	}
 	if (SUCCEEDED(hr))
 	{
-		cout << "client: succeeded getting IX"<< endl;
+		trace("client: succeeded getting IX");
 		pIX->Fx();
+
+		IY* pIY = nullptr;
+		hr = pIX->QueryInterface(IID_IY, (void**)&pIY);
+		if (SUCCEEDED(hr))
+		{
+			trace("client: succeeded getting IY");
+			pIY->Fy();
+			pIY->Release();
+			trace("client: release IY");
+		}
 		pIX->Release();
+		trace("client: release IX");
 	}
-	hr = pIUnknown->Release();
+	::CoUninitialize();
 	return 0;
 }
 
